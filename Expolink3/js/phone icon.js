2c26d2152afcb12a5cae9e065a300db08b8fd9b7
Ahmed hussein <darkrise222@gmail.com>
2017-11-10 08:31:48 +0200

    Initial commit

---
+++ Expolink3/js/phone icon.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+    
+$(document).ready(function()
+{
+    	$("#show").hide(function(){
+			
+			
+		
+}); 
+    
+    $("#click").click(function(){
+                      
+   /*                   
+   $(this).css("color","blue");   
+     $("p").css("color","green");
+	
+       */
+		
+		//$("p").hide();
+		//$(this).hide();
+		
+		  	$("#show").show("slow",function(){
+			
+			
+		
+}); 
+	
+});
+});  
+    
